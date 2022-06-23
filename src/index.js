ce78f6915091aec8f282d3aa27a1e34214bcc750
Elina Razina <elina.razina@dataart.com>
2022-06-23 15:49:04 +0300

    redux from scratch store and action

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function createStore(initialState) {
+  let state = initialState;
+  function getState() {
+    return state;
+  }
+
+  function dispatch(action) {
+    if (action.type === "task/completed") {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (element) => element.id === action.payload.id
+      );
+      newArray[elementIndex].completed = true;
+      state = newArray;
+
+      console.log(state);
+    }
+  }
+  return { getState, dispatch };
+}
+
+const store = createStore([{ id: 1, description: "task1", completed: false }]);
+const App = () => {
+  console.log(store.getState());
+  return (
+    <>
+      <h1>app</h1>
+      <button
+        onClick={() => {
+          store.dispatch({ type: "task/completed", payload: { id: 1 } });
+        }}
+      >
+        Complete
+      </button>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
